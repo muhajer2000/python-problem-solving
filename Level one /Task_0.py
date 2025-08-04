9683caa25d6769fd5c766b3eb0c0e905dab881c6
muhajer <muhajeribrahim4@gmail.com>
2025-08-03 23:18:00 -0700

    task 0 done

---
+++ Level one /Task_0.py
@@ -0,0 +1,6 @@
+# Task_0.py
+# Description: This file contains the solution for a Level 1 Python problem. 
+# It is intended for beginners to practice basic Python concepts and problem-solving skills.
+# Task: Write a simple Python program that prints "Hello, Hacker!" to the console.
+
+print("Hello, Hacker!")  
---
+++ README.md
@@ -0,0 +1,2 @@
+# python problem solving repo
+## so there it 4 level in this repo
