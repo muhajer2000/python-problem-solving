2c42b485369845044af512eb7b02f4359153993a
muhajer <muhajeribrahim4@gmail.com>
2025-08-03 23:29:33 -0700

    task two

---
+++ Level one /Task_1.py
@@ -0,0 +1,4 @@
+# Task 1:  Ask the user for their name and print "Welcome, [name]!"
+
+name = input("Please enter your name: ")   # Accept a name from user and save in var {name}
+print(f"Wellcome, {name}")   
