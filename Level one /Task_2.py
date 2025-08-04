e67a8664384123fd4c39751af1d23e6f10c32d19
muhajer <muhajeribrahim4@gmail.com>
2025-08-03 23:38:07 -0700

    task 3

---
+++ Level one /Task_2.py
@@ -0,0 +1,6 @@
+# Task 2 :  Convert "1337" (string) to an integer and add 10.
+
+string = "1337"
+number = int(string)    # casting the string to int to be able to adding
+
+print(f"the Result: {number + 10}")
---
+++ Level one /Task_3.py
@@ -0,0 +1,9 @@
+# Task 3 :  Create a program that asks for two numbers and prints their sum, difference, product,
+# and quotient
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your sconed number: "))
+print(f"sum: {num1 + num2}")
+print(f"difference: {num1 - num2}")
+print(f"product: {num1 * num2}")
+print(f" quotient: {num1 / num2}")
